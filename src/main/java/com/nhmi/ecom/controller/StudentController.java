21257af1609a81a77422b5b3a469e3d3376fee0f
nhmi <nhmi@netcompany.com>
2023-06-22 18:29:41 +0700

    Add extra api for interracting with db

--- src/main/java/com/nhmi/ecom/controller/StudentController.java
+++ src/main/java/com/nhmi/ecom/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.nhmi.ecom.controller;
 
 import com.nhmi.ecom.student.Student;
-import com.nhmi.ecom.student.StudentService;
+import com.nhmi.ecom.service.StudentService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -34,8 +36,20 @@ public class StudentController {
         return studentService.getStudents();
     }
 
-//    @PostMapping("/addStudent")
-//    public void addStudent(@RequestParam()) {
-//
-//    }
+    @ApiOperation(
+            value="Add a student to database",
+            notes = "Will refactor later for role-based access")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success")
+    })
+    @PostMapping("/addStudent")
+    public String addStudent(@RequestParam(value = "name", required = true) String name,
+                           @RequestParam(value = "email", required = true) String email,
+                           @RequestParam(value = "dob", required = true) String dob) {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate dateOfBirth = LocalDate.parse(dob,fmt);
+        Student student = new Student(name,email, dateOfBirth);
+
+        return studentService.addStudent(student);
+    }
 }
--- src/main/java/com/nhmi/ecom/service/StudentService.java
+++ src/main/java/com/nhmi/ecom/service/StudentService.java
@@ -1,5 +1,7 @@
-package com.nhmi.ecom.student;
+package com.nhmi.ecom.service;
 
+import com.nhmi.ecom.student.Student;
+import com.nhmi.ecom.student.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,22 @@ public class StudentService {
     public StudentService(StudentRepository studentRepository){
         this.studentRepository = studentRepository;
     }
+
     public List<Student> getStudents() {
         return studentRepository.findAll();
     }
 
-    public void addStudent(Student student) {
-        studentRepository.save(student);
+    public String addStudent(Student student) {
+        try {
+            studentRepository.save(student);
+        } catch (Exception e ){
+           throw e;
+        }
+        return "Successfully added a student";
+    }
+
+    public String remove(long id) {
+        studentRepository.deleteById(id);
+        return "Successfully removed a student";
     }
 }
